8dd88338b1d4be6341d0948cc292265bd7051883
saman <abdallaelsaman67@gmail.com>
2019-03-09 17:01:17 +0200

    Initial Commit

---
+++ Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Abdalla Elsaman on 3/7/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Abdalla Elsaman on 3/7/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
